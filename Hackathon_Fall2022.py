fd0e201df086cb6d7e59695122325adbb4ccb6e3
Seth <jonRiemer.88@outlook.com>
2022-10-07 21:44:06 -0400

    testing switching to idea branch

--- Hackathon_Fall2022.py
+++ Hackathon_Fall2022.py
@@ -4,8 +4,41 @@
 # Description:
 
 
+class Recipe:
+
+    def __init__(self, id, name, cooking_time=0):
+        self._id = id
+        self._name = name
+        self._cooking_time = cooking_time
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
+
+    def get_cooking_time(self):
+        return self._cooking_time
+
+
+class Ingredient:
+
+    def __init__(self, id, name):
+        self._id = id
+        self._name = name
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
+
+
+
 def add_recipe():
     pass
+
+
 
 def add_ingrediant():
     pass
@@ -15,6 +48,16 @@ def main():
     choices = input("Enter ingredient")
     pass
 
+def create_log():
+    pass
+
+def generate_random_number():
+    pass
+
+def get_best_match():
+    pass
+
 if __name__ == "__main__":
     main()
+    #test
 
